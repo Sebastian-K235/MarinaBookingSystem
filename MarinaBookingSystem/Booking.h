50c31ad9fd7b59345c53542b5cd57d26e09fd17e
Sebastian Kmiec <sebastian.kmiec235@gmail.com>
2020-04-14 20:04:40 +0100

    Added: booking class node class list class

---
+++ MarinaBookingSystem/Booking.h
@@ -0,0 +1,23 @@
+#include<string>
+#pragma once
+
+
+//Class holding details for bookings
+class Booking
+{
+	//Private attributes
+	float boatLength;
+	float draft;
+	int durationOfStay;
+	float price;
+	std::string ownerName;
+	std::string boatName;
+	std::string typeOfBoat;
+
+	//Public methods
+public:
+	Booking();
+	Booking(float, float, int, float, std::string, std::string, std::string);
+	~Booking();
+};
+
